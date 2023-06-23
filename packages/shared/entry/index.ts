12e91b0e64d890d7fc08089885ea2b5226ad6102
Dan Parfenov <prostodan4ikn@ya.ru>
2023-06-23 19:24:49 +0300

    Add apollo

--- packages/shared/entry/index.ts
+++ packages/shared/entry/index.ts
@@ -1,15 +1,22 @@
 import { createSSRApp } from "vue";
 import { createPinia } from "pinia";
+import { createApolloProvider } from "@vue/apollo-option";
 import { createRouter } from "../../client/src/router";
 import App from "../../client/src/App.vue";
+import { createApolloClient } from "../apollo";
 
-export function createApp() {
-  const pinia = createPinia();
+export function createApp(ssr = false) {
+  const store = createPinia();
   const app = createSSRApp(App);
   const router = createRouter();
+  const apolloClient = createApolloClient(ssr);
+  const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+  });
 
-  app.use(pinia);
+  app.use(store);
   app.use(router);
+  app.use(apolloProvider);
 
-  return { app };
+  return { app, router, store, apolloProvider };
 }
--- packages/shared/entry/server.ts
+++ packages/shared/entry/server.ts
@@ -1,9 +1,29 @@
-import { renderToString } from "vue/server-renderer";
+import { SSRContext, renderToString } from "vue/server-renderer";
+import * as ApolloSSR from "@vue/apollo-ssr";
 import { createApp } from "./";
 
-export async function render() {
-  const { app } = createApp();
-  const ctx = {};
+export async function render(url: string) {
+  const { app, router, store, apolloProvider } = createApp(true);
+
+  // set the router to the desired URL before rendering
+  await router.push(url);
+  await router.isReady();
+
+  const ctx = <SSRContext>{};
+
+  ctx.rendered = () => {
+    // After the app is rendered, our store is now
+    // filled with the state from our components.
+    // When we attach the state to the context, and the `template` option
+    // is used for the renderer, the state will automatically be
+    // serialized and injected into the HTML as `window.__INITIAL_STATE__`.
+    ctx.state = store.state;
+
+    // ALso inject the apollo cache state
+    ctx.apolloState = ApolloSSR.getStates(apolloProvider.clients);
+  };
+
   const html = await renderToString(app, ctx);
+
   return { html };
 }
